4c0c0789d8ed55b1750433fcc374a9175ea61ec2
JingAn Chen <lzsoft.cja@gmail.com>
2018-05-07 15:24:35 -0400

    Allowing all methods by default.

--- avalon-node-router.js
+++ avalon-node-router.js
@@ -3,8 +3,7 @@ const url = require('url');
 const querystring = require('querystring');
 async function start(req, res, environment, map, headers) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Credentials', false);
+    res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Max-Age', '3600');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, Application-Key, Application-Name');
     //
