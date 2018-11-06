3c13c67acf45d3f64d9a7b86554a7c7150b04201
JingAn Chen <lzsoft.cja@gmail.com>
2018-11-05 20:47:05 -0500

    Includes path info in the returning error message.

--- avalon-node-router.js
+++ avalon-node-router.js
@@ -58,7 +58,7 @@ exports.start = async function(req, res, map, headers) {
 				return false;
 		}
 	} else {
-		res.anrEnd(404, "Service does not exist");
+		res.anrEnd(404, "Service does not exist, requesting " + p);
 		return false;
 	}
 };
