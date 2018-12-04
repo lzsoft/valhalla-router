3bf19e55d5e0922a784a06a68a6f484ccfbbb4d1
JingAn Chen <lzsoft.cja@gmail.com>
2018-12-04 17:45:14 -0500

    Code optimization.

--- avalon-node-router.js
+++ avalon-node-router.js
@@ -2,13 +2,14 @@ const {
 	URL
 } = require('url');
 const querystring = require('querystring');
-exports.start = async function(req, res, map, headers) {
+exports.start = async function(req, res, routerMap, responseHeaders) {
+	// Default response headers
 	res.setHeader('Access-Control-Allow-Methods', 'GET, OPTION, POST, PUT, DELETE, PATCH');
 	res.setHeader('Access-Control-Max-Age', '3600');
 	res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
-	//
-	for (const h in headers) {
-		res.setHeader(h, headers[h]);
+	// Custom response headers
+	for (const h in responseHeaders) {
+		res.setHeader(h, responseHeaders[h]);
 	}
 	//
 	res.anrEnd = function(statusCode, body = {}, headers) {
@@ -29,13 +30,15 @@ exports.start = async function(req, res, map, headers) {
 		}
 		return true;
 	};
+	// OPTION method auto succeed
 	if (req.method === "OPTIONS") {
 		res.anrEnd(200);
 		return true;
 	}
+	// Start router mapping
 	let u = new URL(req.url, 'http://127.0.0.1');
 	let p = null;
-	for (const m in map) {
+	for (const m in routerMap) {
 		if (u.pathname.indexOf(m) === 0) {
 			let up = u.pathname.replace(m, '');
 			if (up[0] === '/' || up[0] === '#' || up[0] === '?' || !up[0]) {
@@ -43,50 +46,44 @@ exports.start = async function(req, res, map, headers) {
 			}
 		}
 	}
-	if (p && map[p][req.method]) {
+	// Handler hit
+	if (p && routerMap[p][req.method]) {
 		switch (true) {
 			case (req.method === 'GET'):
-				await map[p][req.method](req, res, querystring.parse(u.search.substr(1)));
+				await routerMap[p][req.method](req, res, querystring.parse(u.search.substr(1)));
 				return true;
 			case (req.headers['content-type'] && req.headers['content-type'] === 'application/json'):
-				await map[p][req.method](req, res, await processJson(req, res));
+				await routerMap[p][req.method](req, res, await processJson(req, res));
 				return true;
 			case (req.headers['content-type'] && req.headers['content-type'] !== 'application/json'):
-				await map[p][req.method](req, res);
+				await routerMap[p][req.method](req, res);
 				return true;
 			default:
 				res.anrEnd(400, "You must provide a valid content-type header");
 				return false;
 		}
 	} else {
-		res.anrEnd(496);
+		res.anrEnd(496, 'Request path or method is undefined');
 		return false;
 	}
 };
-async function processJson(req, res) {
+async function processJson(req) {
 	return new Promise(function(resolve, reject) {
-		try {
-			let text = '';
-			let json = {};
-			req.on('data', (chunk) => {
-				text += chunk;
-			});
-			req.on('end', () => {
-				try {
-					json = JSON.parse(text) || {};
-					resolve(json);
-				} catch (e) {
-					res.statusCode = 400;
-					res.statusMessage = "The incoming JSON string is invalid";
-					res.end();
-					reject(e);
-				}
-			});
-		} catch (e) {
-			res.statusCode = 400;
-			res.statusMessage = "The incoming JSON string is invalid";
-			res.end();
+		let text = '';
+		let json = {};
+		req.on('data', chunk => {
+			text += chunk;
+		});
+		req.on('end', () => {
+			try {
+				json = JSON.parse(text) || {};
+				resolve(json);
+			} catch (e) {
+				reject(new Error('Incoming request is not a valid JSON'));
+			}
+		});
+		req.on('error', e => {
 			reject(e);
-		}
+		});
 	});
 }
